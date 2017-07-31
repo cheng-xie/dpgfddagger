65d0c682056163b6d5ca7dc75dad46672ea27848
ChengXie <cheng.k.xie@gmail.com>
2017-07-31 08:30:07 -0700

    Working on mimic agent

--- mimic_agent.py
+++ mimic_agent.py
@@ -3,12 +3,9 @@ import torch.optim as opt
 from torch.autograd import Variable
 from torch import FloatTensor as FT
 import agent
-from replay_buffer import ExperienceReplay
 import numpy as np
 from torch.utils.serialization import load_lua
 import importlib.util
-# import model_defs.ddpg_models.mountain_cart.actor as actor
-# import model_defs.ddpg_models.mountain_cart.critic as critic
 import random
 
 #Default hyperparameter values
@@ -19,36 +16,15 @@ BATCH_SIZE = 100
 class MimicAgent():
     """An agent that mimics another agent
 
-
-    The agent stores a replay buffer along with
-    two models of the data, an actor and a critic.
-
     Attributes:
-        auxiliary_losses: The list of enabled
-        auxiliary rewards for this agent
-
         actor: The actor model that takes a state
         and returns a new action.
-
-        critic: The critic model that takes a state
-        and an action and returns the expected
-        reward
-
-        replay_buffer: The DDPGAgent replay buffer
     """
 
     """
     @property
     def actor(self):
         return self.actor
-
-    @property
-    def critic(self):
-        return self.critic
-
-    @property
-    def replay_buffer(self):
-        return self.replay_buffer
     """
 
     def __init__(self,
@@ -66,11 +42,6 @@ class MimicAgent():
             buffer_size: size of the replay buffer
 
             alpha: The learning rate
-
-            gamma: The discount factor
-
-        Returns:
-            A DDPGAgent object
         """
         self._use_cuda = use_cuda
 
@@ -102,9 +73,7 @@ class MimicAgent():
         """Trains the agent for a bit.
 
             Args:
-                None
             Returns:
-                None
         """
         dataset = TensorDataset(FT(states), FT(actions))
         self.dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
@@ -113,9 +82,6 @@ class MimicAgent():
         log_iters_count = 0
         for epoch in range(epochs):
             for cur_iter, (state_batch, action_batch) in enumerate(self.dataloader):
-                # Zero out the gradients
-                self.actor.zero_grad()
-
                 # Cuda
                 if self.use_cuda:
                     state_batch = state_batch.cuda()
@@ -126,14 +92,16 @@ class MimicAgent():
                 pred_actions = self.actor.forward(state_batch)
 
                 # Calculate the loss
-                loss = self.criterion(recon, target)
+                loss = self.criterion(pred_actions, action_batch)
+
+                # Zero out the gradients
+                self._actor_optimizer.zero_grad()
 
                 # Calculate some gradients
                 loss.backward()
 
                 # Run update step
-                self.enc_optim.step()
-                self.dec_optim.step()
+                self._actor_optimizer.step()
 
                 tot_loss += loss.data[0]
                 log_iters_count += 1
@@ -143,44 +111,11 @@ class MimicAgent():
                     print('Loss', tot_loss/iters_per_log)
                     tot_loss = 0
 
-        #update_actor
-        for i in range(self._actor_iter_count):
-            s_t, a_t, r_t, s_t1, done = self.replay_buffer.batch_sample(self._batch_size)
-            done = self.upcast(done)
-            s_t = self.upcast(s_t)
-            a_t = self.upcast(a_t)
-            s_t1 = self.upcast(s_t1)
-            r_t = self.upcast(r_t)
-            a_t1,aux_actions = self.actor.forward(s_t1,self.auxiliary_losses.keys())
-            expected_reward = self.critic.forward(s_t1,a_t1)
-
-            total_loss = -1*expected_reward
-            for key,aux_reward_tuple in self.auxiliary_losses.items():
-                aux_weight,aux_module = aux_reward_tuple
-                total_loss += aux_weight*aux_module(aux_actions[key],s_t,a_t,r_t,s_t1,a_t1)
-
-            mean_loss = torch.mean(total_loss)
-
-            #print('LOSS:', mean_loss, 'Eps', self.epsilon)
-            #preform one optimization update
-            self._actor_optimizer.zero_grad()
-            mean_loss.backward()
-            self._actor_optimizer.step()
-
     def get_next_action(self,
-            cur_state,
-            agent_id=None):
+            cur_state):
         """Get the next action from the agent.
-
-            Takes a state,reward and possibly auxiliary reward
-            tuple and returns the next action from the agent.
-
             Args:
                 cur_state: The current state of the enviroment
-                prev_reward: The previous reward from the enviroment
-                is_done: Signals if a given episode is done.
-                is_test: Check to see if the agent is done
-                agent_id=None
             Returns:
                 The next action that the agent with the given
                 agent_id will carry out given the current state
